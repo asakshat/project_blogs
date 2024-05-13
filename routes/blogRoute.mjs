e427447e28676ce3275c37eb5c417ae536b9475f
asakshat <asakshat453@gmail.com>
2024-05-13 14:12:26 +0200

    delete message function

--- routes/blogRoute.mjs
+++ routes/blogRoute.mjs
@@ -1,11 +1,10 @@
 import express from 'express';
-import {blogPost,blogUpdate,getBlog} from '../controllers/blogController.mjs';
+import {blogPost,blogUpdate} from '../controllers/blogController.mjs';
 const router = express.Router();
 
 
 router.post('/post', (blogPost));
 router.patch('/update', (blogUpdate));
-router.get('/getposts', (getBlog));
 
 export default router;
 
